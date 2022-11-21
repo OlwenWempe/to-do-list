c0faa2bff7cead8db275703421aa3fbdfc64d498
Olwen WEMPE <wempe.olwen@gmail.com>
2022-11-21 19:18:55 +0100

    major upgrade from procedural coding to OOP

--- register.php
+++ register.php
@@ -1,74 +1,50 @@
 <?php
-session_start();
-if (isset($_SESSION['authenticated'])) {
-    header('Location: to-do.php');
-} else {
-    $title = 'Inscription';
-    if (
-        isset($_POST['last_name'])
-        && isset($_POST['first_name'])
-        && isset($_POST['email'])
-        && isset($_POST['password'])
-        && !empty($_POST['last_name'])
-        && !empty($_POST['first_name'])
-        && !empty($_POST['email'])
-        && !empty($_POST['password'])
-    ) {
-        try {
-            $pdo = new PDO(
-                "mysql:host=localhost;dbname=to-do-list",
-                "root",
-                "Logan24092018@",
-                [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
-                ]
+require_once "partials/_check_if_unlogged.php";
+require_once "models/User.php";
 
-            );
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-            echo "erreur";
-            exit;
+$title = 'Inscription';
+if (isset($_POST['submit'])) {
+    require_once '_start_session.php';
+
+    //check if email field is filled in
+    if (empty($_POST['email'])) {
+        $_SESSION['errors'][] = "veuillez remplir tous les champs";
+    } else {
+        //check if data is valid
+        $secured_data = [
+            `last_name` => htmlspecialchars($_POST['last_name']),
+            `first_name` => htmlspecialchars($_POST['first_name']),
+            `email` => htmlspecialchars($_POST['email']),
+            `password` => htmlspecialchars($_POST['password'])
+        ];
+
+        //Check email integrity
+        try {
+            $user = new User();
+            $user->setEmail($secured_data['email']);
+            $user->setPassword($secured_data['password']);
         } catch (Exception $e) {
+            $_SESSION['errors'][] = $e->getMessage();
         }
-        try {
-            $stmt = $pdo->prepare("SELECT `first_name` FROM user WHERE email = :email");
-            $stmt->execute(['email' => $_POST['email']]);
-            $id_exists = $stmt->fetch();
-            if ($id_exists != false) {
+        if (empty($_SESSION['errors'])) {
 
-                $_SESSION['error'] = "Vous êtes déjà inscrit chez nous avec cet email";
-            } else {
-                try {
-                    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-                    $stmt = $pdo->prepare("INSERT INTO user (last_name, first_name, email, password)
-         VALUES (:last_name, :first_name, :email, :password)");
-
-                    $stmt->execute([
-                        'last_name' => $_POST['last_name'],
-                        'first_name' => $_POST['first_name'],
-                        'email' => $_POST['email'],
-                        'password' => $password
-
-                    ]);
-
-                    $_SESSION['first_name'] = $_POST['first_name'];
-                    $_SESSION['email'] = $_POST['email'];
-                    $_SESSION['password'] = $password;
-                } catch (PDOException $th) {
-                    echo $th->getMessage();
-                    echo "erreur";
-                    exit;
-                } catch (Exception $th) {
-                }
-
-                unset($pdo);
-
-                header('Location: login.php');
+            if ($user->isExistent()) {
+                $_SESSION['errors'][] = "cette adresse mail existe déjà";
             }
-        } catch (PDOException $th) {
-            echo $th->getMessage();
         }
-    };
-    require_once './views/register.view.php';
-}
+
+        if (empty($_SESSION['errors'])) {
+
+            try {
+                $user->insert();
+                $_SESSION['first_name'] = $_POST['first_name'];
+                $_SESSION['email'] = $_POST['email'];
+                header('Location: login.php');
+                exit;
+            } catch (Exception $e) {
+                $_SESSION['errors'][] = $e->getMessage();
+            }
+        }
+    }
+};
+require_once './views/register.view.php';
