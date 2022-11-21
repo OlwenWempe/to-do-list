d91c75be454b1a9cf0a8df7ccdb249ded96ea668
Olwen WEMPE <wempe.olwen@gmail.com>
2022-11-21 23:29:48 +0100

    added new partial

---
+++ partials/_check_if_known.php
@@ -0,0 +1,7 @@
+<?php
+if (isset($_SESSION['user']['first_name'])) {
+    $first_name = ($_SESSION['user']['first_name']);
+} else {
+    $first_name = 'chèr(e) inconnu(e)';
+    $_SESSION['error'][] = `Il semblerait que vous avez oublié de vous identifier`;
+}
