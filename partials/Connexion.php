4c012afa2d755512551c0105b48229434b266c66
Olwen WEMPE <wempe.olwen@gmail.com>
2022-11-21 17:34:21 +0100

    update

--- partials/Connexion.php
+++ partials/Connexion.php
@@ -14,7 +14,11 @@ class Connexion
             $this->pdo = new PDO(
                 "mysql:host=" . $db_config['db_host'] . ";dbname=" . $db_config['db_name'],
                 $db_config['db_user'],
-                $db_config['db_password']
+                $db_config['db_password'],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+                ]
             );
         } catch (PDOException $e) {
             echo $e->getMessage();
